bec817649253def641535b55c69d94fdda9cabd9
Supatchaya <tai.pakdee@gmail.com>
2021-07-22 08:32:04 -0600

    Completed unit #4

--- src/app/modules/material/app-routing.module.ts
+++ src/app/modules/material/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnalyticsComponent } from 'src/app/components/analytics/analytics.component';
+import { DepartmentsComponent } from 'src/app/components/departments/departments.component';
+import { TimesheetComponent } from 'src/app/components/timesheet/timesheet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '',   redirectTo: 'departments', pathMatch: 'full' },
+    { path: 'departments', component: DepartmentsComponent },
+    { path: 'timesheet', component: TimesheetComponent },
+    { path: 'analytics', component: AnalyticsComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
